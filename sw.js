f80bf161d340de6f0a1b595b0c84ff15c4a4b243
rubenmanchatrans-design <ruben.manchatrans@gmail.com>
2026-06-22 10:07:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
-self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('install', e => {
+  // No activar automáticamente: esperar a que el usuario confirme la actualización
+});
+
+self.addEventListener('message', e => {
+  if (e.data && e.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
